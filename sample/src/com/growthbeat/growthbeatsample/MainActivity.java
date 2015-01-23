afa92b10485a76505ad836c944c4f3c8cb5e0811
Naoyuki Kataoka <kataokanaoyuki@gmail.com>
2015-01-23 18:33:41 +0900

    Fix implementation of sample application

--- sample/src/com/growthbeat/growthbeatsample/MainActivity.java
+++ sample/src/com/growthbeat/growthbeatsample/MainActivity.java
@@ -6,7 +6,6 @@ import android.view.Menu;
 
 import com.growthbeat.Growthbeat;
 import com.growthbeat.analytics.GrowthAnalytics;
-import com.growthpush.model.Environment;
 
 public class MainActivity extends Activity {
 
@@ -14,10 +13,8 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		Growthbeat.getInstance().initialize(getApplicationContext(), "OyVa3zboPjHVjsDC", "3EKydeJ0imxJ5WqS22FJfdVamFLgu7XA");
-		Growthbeat.getInstance().initializeGrowthPush(BuildConfig.DEBUG ? Environment.development : Environment.production, "955057365401");
-		Growthbeat.getInstance().initializeGrowthReplay();
-		GrowthAnalytics.getInstance().getHttpClient().setBaseUrl("http://api.stg.analytics.growthbeat.com/");
+		Growthbeat.getInstance().initialize(this, "OyVa3zboPjHVjsDC", "3EKydeJ0imxJ5WqS22FJfdVamFLgu7XA");
+		Growthbeat.getInstance().initializeGrowthAnalytics();
 	}
 
 	@Override
