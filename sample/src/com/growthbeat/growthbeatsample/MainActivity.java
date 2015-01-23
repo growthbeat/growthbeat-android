2cf858f399bcf3e77c50472f4eec1a77c2071c24
Naoyuki Kataoka <kataokanaoyuki@gmail.com>
2015-01-23 18:07:29 +0900

    Build growthbeat.jar and use it in sample

--- sample/src/com/growthbeat/growthbeatsample/MainActivity.java
+++ sample/src/com/growthbeat/growthbeatsample/MainActivity.java
@@ -2,10 +2,11 @@ package com.growthbeat.growthbeatsample;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.os.Environment;
 import android.view.Menu;
 
 import com.growthbeat.Growthbeat;
+import com.growthbeat.analytics.GrowthAnalytics;
+import com.growthpush.model.Environment;
 
 public class MainActivity extends Activity {
 
@@ -13,9 +14,25 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		Growthbeat.getInstance().initialize(this.getApplicationContext(), "dy6VlRMnN3juhW9L", "NuvkVhQtRDG2nrNeDzHXzZO5c6j0Xu5t");
+		Growthbeat.getInstance().initialize(getApplicationContext(), "OyVa3zboPjHVjsDC", "3EKydeJ0imxJ5WqS22FJfdVamFLgu7XA");
 		Growthbeat.getInstance().initializeGrowthPush(BuildConfig.DEBUG ? Environment.development : Environment.production, "955057365401");
 		Growthbeat.getInstance().initializeGrowthReplay();
+	}
+
+	@Override
+	public void onStart() {
+
+		super.onStart();
+
+		GrowthAnalytics.getInstance().open();
+		GrowthAnalytics.getInstance().setBasicTags();
+
+	}
+
+	@Override
+	public void onStop() {
+		super.onStop();
+		GrowthAnalytics.getInstance().close();
 	}
 
 	@Override
