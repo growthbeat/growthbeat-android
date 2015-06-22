d2eac98d2062741fc50f22f17370f42bc3a550b5
Ogawa Shigeru <ogawa@sirok.co.jp>
2015-06-22 18:38:19 +0900

    setJsonObject

--- source/GrowthPush/src/com/growthpush/model/Event.java
+++ source/GrowthPush/src/com/growthpush/model/Event.java
@@ -3,9 +3,11 @@ package com.growthpush.model;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import com.growthbeat.model.Model;
+import com.growthbeat.utils.JSONObjectUtils;
 import com.growthpush.GrowthPush;
 
 public class Event extends Model {
@@ -30,20 +32,70 @@ public class Event extends Model {
 		params.put("name", name);
 		if (value != null)
 			params.put("value", value);
-		JSONObject jsonObject = GrowthPush.getInstance().getHttpClient().post("3/tags", params);
+		JSONObject jsonObject = GrowthPush.getInstance().getHttpClient().post("3/events", params);
 		return new Event(jsonObject);
+	}
+
+	public int getGoalId() {
+		return goalId;
+	}
+
+	public void setGoalId(int goalId) {
+		this.goalId = goalId;
+	}
+
+	public long getClientId() {
+		return clientId;
+	}
+
+	public void setClientId(long clientId) {
+		this.clientId = clientId;
+	}
+
+	public long getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(long timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
 	}
 
 	@Override
 	public JSONObject getJsonObject() {
-		// TODO Auto-generated method stub
-		return null;
+		JSONObject jsonObject = new JSONObject();
+
+		try {
+			jsonObject.put("goalId", getGoalId());
+			jsonObject.put("clientId", getClientId());
+			jsonObject.put("timestamp", getTimestamp());
+			jsonObject.put("value", getValue());
+		} catch (JSONException e) {
+		}
+
+		return jsonObject;
 	}
 
 	@Override
 	public void setJsonObject(JSONObject jsonObject) {
-		// TODO Auto-generated method stub
-
+		try {
+			if (JSONObjectUtils.hasAndIsNotNull(jsonObject, "goalId"))
+				setGoalId(jsonObject.getInt("goalId"));
+			if (JSONObjectUtils.hasAndIsNotNull(jsonObject, "clientId"))
+				setClientId(jsonObject.getLong("clientId"));
+			if (JSONObjectUtils.hasAndIsNotNull(jsonObject, "timestamp"))
+				setTimestamp(jsonObject.getLong("timestamp"));
+			if (JSONObjectUtils.hasAndIsNotNull(jsonObject, "value"))
+				setValue(jsonObject.getString("value"));
+		} catch (JSONException e) {
+		}
 	}
 
 }
--- source/GrowthPush/src/com/growthpush/model/Tag.java
+++ source/GrowthPush/src/com/growthpush/model/Tag.java
@@ -3,16 +3,18 @@ package com.growthpush.model;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import com.growthbeat.model.Model;
+import com.growthbeat.utils.JSONObjectUtils;
 import com.growthpush.GrowthPush;
 
 public class Tag extends Model {
 
 	private int tagId;
 
-	private int clientId;
+	private long clientId;
 
 	private String value;
 
@@ -32,16 +34,55 @@ public class Tag extends Model {
 		return new Tag(jsonObject);
 	}
 
+	public int getTagId() {
+		return tagId;
+	}
+
+	public void setTagId(int tagId) {
+		this.tagId = tagId;
+	}
+
+	public long getClientId() {
+		return clientId;
+	}
+
+	public void setClientId(long clientId) {
+		this.clientId = clientId;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
 	@Override
 	public JSONObject getJsonObject() {
-		// TODO Auto-generated method stub
-		return null;
+		JSONObject jsonObject = new JSONObject();
+		try {
+			jsonObject.put("tagId", getTagId());
+			jsonObject.put("clientId", getClientId());
+			jsonObject.put("value", getValue());
+		} catch (JSONException e) {
+		}
+
+		return jsonObject;
 	}
 
 	@Override
 	public void setJsonObject(JSONObject jsonObject) {
-		// TODO Auto-generated method stub
+
+		try {
+			if (JSONObjectUtils.hasAndIsNotNull(jsonObject, "tagId"))
+				setTagId(jsonObject.getInt("tagId"));
+			if (JSONObjectUtils.hasAndIsNotNull(jsonObject, "clientId"))
+				setClientId(jsonObject.getLong("clientId"));
+			if (JSONObjectUtils.hasAndIsNotNull(jsonObject, "value"))
+				setValue(jsonObject.getString("value"));
+		} catch (JSONException e) {
+		}
 
 	}
-
 }
