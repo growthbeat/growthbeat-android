e43e233fe45c272b8d03077581cb939ad1e92b72
TABATAKATSUTOSHI <t.katsu22@gmail.com>
2015-07-24 00:27:13 +0900

    get logger from GrowthBeat

--- source/GrowthLink/src/com/growthbeat/link/InstallReferrerReceiver.java
+++ source/GrowthLink/src/com/growthbeat/link/InstallReferrerReceiver.java
@@ -7,11 +7,9 @@ import android.content.Context;
 import android.content.Intent;
 
 public class InstallReferrerReceiver extends BroadcastReceiver {
-	private static final String LOGGER_DEFAULT_TAG = "InstallReferrerReceiver";
-	private final Logger logger = new Logger(LOGGER_DEFAULT_TAG);
 	@Override
 	public void onReceive(Context context, Intent intent) {
-		logger.info("InstallReferrerReceiver onReceive called");
+		GrowthLink.getInstance().getLogger().info("InstallReferrerReceiver onReceive called");
 		GrowthLink.getInstance().getInstallReferrerReceiveHandler().onReceive(context, intent);
 	}
 }
