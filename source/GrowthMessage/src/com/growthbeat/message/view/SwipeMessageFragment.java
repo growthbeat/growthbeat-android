989cc68468625de2d2609e960bbe674bbcd3c693
Watanabe Shotaro <watanabe_shotaro@sirok.co.jp>
2015-08-03 16:26:30 +0900

    Refactor show image and buttons

--- source/GrowthMessage/src/com/growthbeat/message/view/SwipeMessageFragment.java
+++ source/GrowthMessage/src/com/growthbeat/message/view/SwipeMessageFragment.java
@@ -116,31 +116,12 @@ public class SwipeMessageFragment extends Fragment {
 			layoutParams.gravity = android.view.Gravity.FILL;
 			frameLayout.setLayoutParams(layoutParams);
 
-			ImageView imageView = new ImageView(getActivity());
-			FrameLayout.LayoutParams imageLayoutParams = new FrameLayout.LayoutParams(imageRect.getWidth(),
-					imageRect.getHeight());
-			imageLayoutParams.leftMargin = imageRect.getLeft();
-			imageLayoutParams.topMargin = imageRect.getTop();
-			imageView.setLayoutParams(imageLayoutParams);
-			imageView.setScaleType(ScaleType.CENTER_INSIDE);
-			imageView.setImageBitmap(cachedImages.get(picture.getUrl()));
-			frameLayout.addView(imageView);
+			frameLayout.addView(createImage(picture, imageRect));
 
-			if (buttons.size() > i) {
-				Button button = buttons.get(i);
-				final ImageButton imageButton = (ImageButton) button;
-				TouchableImageView touchableImageView = new TouchableImageView(getActivity());
-				touchableImageView.setScaleType(ScaleType.FIT_CENTER);
-				touchableImageView.setOnClickListener(new View.OnClickListener() {
-					@Override
-					public void onClick(View v) {
-						GrowthMessage.getInstance().selectButton(imageButton, swipeMessage);
-						if (!getActivity().isFinishing())
-							getActivity().finish();
-					}
-				});
-				touchableImageView.setImageBitmap(cachedImages.get(imageButton.getPicture().getUrl()));
-				frameLayout.addView(wrapViewWithAbsoluteLayout(touchableImageView, buttonRect));
+			if (swipeMessage.getSwipeType().equals(SwipeType.buttons) && buttons.size() > i) {
+				View buttonView = createButton(buttons.get(i), buttonRect);
+				if (buttonView != null)
+					frameLayout.addView(buttonView);
 			}
 
 			adapter.add(frameLayout);
@@ -163,28 +144,9 @@ public class SwipeMessageFragment extends Fragment {
 		if (buttons.size() != 1)
 			return;
 
-		Button button = buttons.get(0);
-
-		switch (button.getType()) {
-		case image:
-			final ImageButton imageButton = (ImageButton) button;
-
-			TouchableImageView touchableImageView = new TouchableImageView(getActivity());
-			touchableImageView.setScaleType(ScaleType.FIT_CENTER);
-			touchableImageView.setOnClickListener(new View.OnClickListener() {
-				@Override
-				public void onClick(View v) {
-					GrowthMessage.getInstance().selectButton(imageButton, swipeMessage);
-					if (!getActivity().isFinishing())
-						getActivity().finish();
-				}
-			});
-			touchableImageView.setImageBitmap(cachedImages.get(imageButton.getPicture().getUrl()));
-
-			innerLayout.addView(wrapViewWithAbsoluteLayout(touchableImageView, rect));
-			break;
-		default:
-			break;
+		View buttonView = createButton(buttons.get(0), rect);
+		if (buttonView != null) {
+			innerLayout.addView(buttonView);
 		}
 	}
 
@@ -209,6 +171,40 @@ public class SwipeMessageFragment extends Fragment {
 		touchableImageView.setImageBitmap(cachedImages.get(closeButton.getPicture().getUrl()));
 
 		innerLayout.addView(wrapViewWithAbsoluteLayout(touchableImageView, rect));
+	}
+
+	private View createImage(Picture picture, Rect rect) {
+		ImageView imageView = new ImageView(getActivity());
+		FrameLayout.LayoutParams imageLayoutParams = new FrameLayout.LayoutParams(rect.getWidth(), rect.getHeight());
+		imageLayoutParams.leftMargin = rect.getLeft();
+		imageLayoutParams.topMargin = rect.getTop();
+		imageView.setLayoutParams(imageLayoutParams);
+		imageView.setScaleType(ScaleType.CENTER_INSIDE);
+		imageView.setImageBitmap(cachedImages.get(picture.getUrl()));
+
+		return imageView;
+	}
+
+	private View createButton(Button button, Rect rect) {
+		switch (button.getType()) {
+		case image:
+			final ImageButton imageButton = (ImageButton) button;
+
+			TouchableImageView touchableImageView = new TouchableImageView(getActivity());
+			touchableImageView.setScaleType(ScaleType.FIT_CENTER);
+			touchableImageView.setOnClickListener(new View.OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					GrowthMessage.getInstance().selectButton(imageButton, swipeMessage);
+					if (!getActivity().isFinishing())
+						getActivity().finish();
+				}
+			});
+			touchableImageView.setImageBitmap(cachedImages.get(imageButton.getPicture().getUrl()));
+			return wrapViewWithAbsoluteLayout(touchableImageView, rect);
+		default:
+			return null;
+		}
 	}
 
 	private View wrapViewWithAbsoluteLayout(View view, Rect rect) {
