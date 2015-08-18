45a7d123260500e7aeeb149a81b40bd217e97a5b
Naoyuki Kataoka <kataokanaoyuki@gmail.com>
2015-08-19 02:32:25 +0900

    Synchronize with both of install referrer and cookie tracking in SynchronizationCallback

--- source/GrowthLink/src/com/growthbeat/link/callback/DefaultSynchronizationCallback.java
+++ source/GrowthLink/src/com/growthbeat/link/callback/DefaultSynchronizationCallback.java
@@ -10,39 +10,59 @@ import com.growthbeat.utils.DeviceUtils;
 
 public class DefaultSynchronizationCallback implements SynchronizationCallback {
 
-	@Override
-	public void onComplete(Synchronization synchronization) {
+	private static final long INSTALL_REFERRER_TIMEOUT = 10 * 1000;
 
-		if (!synchronization.getBrowser())
-			return;
+	@Override
+	public void onComplete(final Synchronization synchronization) {
 
 		new Thread(new Runnable() {
 			@Override
 			public void run() {
 
-				String urlString = GrowthLink.getInstance().getSyncronizationUrl() + "?applicationId="
-						+ GrowthLink.getInstance().getApplicationId();
-				try {
-					String advertisingId = DeviceUtils.getAdvertisingId().get();
-					if (advertisingId != null) {
-						urlString += "&advertisingId=" + advertisingId;
-					}
-				} catch (Exception e) {
-					GrowthLink.getInstance().getLogger().warning("Failed to get advertisingId: " + e.getMessage());
+				String installReferrer = GrowthLink.getInstance().waitInstallReferrer(INSTALL_REFERRER_TIMEOUT);
+				if (installReferrer != null && installReferrer.length() != 0) {
+					synchronizeWithInstallReferrer(installReferrer);
+					return;
 				}
 
-				Uri uri = Uri.parse(urlString);
-				final android.content.Intent androidIntent = new android.content.Intent(android.content.Intent.ACTION_VIEW, uri);
-				androidIntent.setFlags(android.content.Intent.FLAG_ACTIVITY_NEW_TASK);
+				if (synchronization.getBrowser())
+					synchronizeWithCookieTracking();
 
-				new Handler(Looper.getMainLooper()).post(new Runnable() {
-					public void run() {
-						GrowthLink.getInstance().getContext().startActivity(androidIntent);
-					}
-				});
+				installReferrer = GrowthLink.getInstance().waitInstallReferrer(Long.MAX_VALUE);
+				synchronizeWithInstallReferrer(installReferrer);
 
 			}
-		}).start();
+		});
+
+	}
+
+	protected void synchronizeWithInstallReferrer(String installReferrer) {
+		String uriString = "?" + installReferrer.replace("growthlink.clickId", "clickId").replace("growthbeat.uuid", "uuid");
+		GrowthLink.getInstance().handleOpenUrl(Uri.parse(uriString));
+	}
+
+	protected void synchronizeWithCookieTracking() {
+
+		String urlString = GrowthLink.getInstance().getSyncronizationUrl() + "?applicationId="
+				+ GrowthLink.getInstance().getApplicationId();
+		try {
+			String advertisingId = DeviceUtils.getAdvertisingId().get();
+			if (advertisingId != null) {
+				urlString += "&advertisingId=" + advertisingId;
+			}
+		} catch (Exception e) {
+			GrowthLink.getInstance().getLogger().warning("Failed to get advertisingId: " + e.getMessage());
+		}
+
+		Uri uri = Uri.parse(urlString);
+		final android.content.Intent androidIntent = new android.content.Intent(android.content.Intent.ACTION_VIEW, uri);
+		androidIntent.setFlags(android.content.Intent.FLAG_ACTIVITY_NEW_TASK);
+
+		new Handler(Looper.getMainLooper()).post(new Runnable() {
+			public void run() {
+				GrowthLink.getInstance().getContext().startActivity(androidIntent);
+			}
+		});
 
 	}
 
