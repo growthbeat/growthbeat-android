87ad54e671af9c67aff7541864dea3905770ec52
sangotaro <sango0715@gmail.com>
2016-06-30 12:26:26 +0900

    Update sample app

--- sample/src/main/java/com/growthbeat/sample/MainActivity.java
+++ sample/src/main/java/com/growthbeat/sample/MainActivity.java
@@ -17,97 +17,66 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-		GrowthPush.getInstance().getHttpClient().setBaseUrl("https://api.stg.growthpush.com/");
-		GrowthPush.getInstance().initialize(this, "PIaD6TaVt7wvKwao",  "RtYOQo4QaSaFHNYdZSddSeoeEiJ2kboW",
-				BuildConfig.DEBUG ? Environment.development : Environment.production);
-		GrowthLink.getInstance().initialize(this, "PIaD6TaVt7wvKwao", "FD2w93wXcWlb68ILOObsKz5P3af9oVMo");
-		GrowthPush.getInstance().requestRegistrationId("186415479559");
-		GrowthLink.getInstance().handleOpenUrl(getIntent().getData());
+        GrowthPush.getInstance().initialize(this, "PIaD6TaVt7wvKwao", "FD2w93wXcWlb68ILOObsKz5P3af9oVMo",
+            BuildConfig.DEBUG ? Environment.development : Environment.production);
+        GrowthPush.getInstance().requestRegistrationId("186415479559");
 
-		GrowthPush.getInstance().trackEvent("Launch");
+        GrowthLink.getInstance().initialize(this, "PIaD6TaVt7wvKwao", "FD2w93wXcWlb68ILOObsKz5P3af9oVMo");
+        GrowthLink.getInstance().handleOpenUrl(getIntent().getData());
 
-		new Thread(new Runnable() {
-			@Override
-			public void run() {
-				Client client = Growthbeat.getInstance().waitClient();
-				Log.d("GrowthbeatSample", String.format("clientId is %s", client.getId()));
-			}
-		}).start();
+        GrowthPush.getInstance().trackEvent("Launch");
 
-		GrowthPush.getInstance().trackEvent("ReceiveMessage", null, new ShowMessageHandler() {
-			@Override
-			public void complete(MessageRenderHandler renderHandler) {
-				renderHandler.render();
-			}
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Client client = Growthbeat.getInstance().waitClient();
+                Log.d("GrowthbeatSample", String.format("clientId is %s", client.getId()));
+            }
+        }).start();
 
-			@Override
-			public void error(String error) {
+        GrowthPush.getInstance().trackEvent("AllowPushPermission");
 
-			}
-		});
+        findViewById(R.id.set_tag_button).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    EditText nameEditText = (EditText) findViewById(R.id.tag_name_text);
+                    EditText valueEditText = (EditText) findViewById(R.id.tag_value_text);
 
-		GrowthPush.getInstance().setTag("tag1", "TAG");
-		GrowthPush.getInstance().trackEvent("AllowPushPermission");
+                    GrowthPush.getInstance().setTag(nameEditText.getText().toString(), valueEditText.getText().toString());
+                } catch (NumberFormatException e) {
+                    Log.w("Grwothbeat Sample", "Input value error :" + e.getMessage());
+                }
+            }
+        });
 
-		findViewById(R.id.random_tag_button).setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				// TODO: add message2.0 event
-				// GrowthAnalytics.getInstance().setRandom();
-			}
-		});
+        findViewById(R.id.track_event_button).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    EditText nameEditText = (EditText) findViewById(R.id.event_name_text);
+                    EditText valueEditText = (EditText) findViewById(R.id.event_value_text);
 
-		findViewById(R.id.development_tag_check_box).setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				CheckBox checkBox = (CheckBox) v;
-				// TODO: add message2.0 event
-				// GrowthAnalytics.getInstance().setDevelopment(checkBox.isChecked());
-			}
-		});
+                    GrowthPush.getInstance().trackEvent(nameEditText.getText().toString(), valueEditText.getText().toString());
+                } catch (NumberFormatException e) {
+                    Log.w("Grwothbeat Sample", "Input value error :" + e.getMessage());
+                }
+            }
+        });
+    }
 
-		findViewById(R.id.level_tag_button).setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				try {
-					EditText editText = (EditText) findViewById(R.id.level_edit_text);
-					// TODO: add message2.0 event
-					// GrowthAnalytics.getInstance().setLevel(Integer.valueOf(editText.getText().toString()));
-				} catch (NumberFormatException e) {
-					Log.w("Grwothbeat Sample", "Input value error :" + e.getMessage());
-				}
-			}
-		});
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
 
-		findViewById(R.id.purchase_event_button).setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				try {
-					EditText priceEditText = (EditText) findViewById(R.id.price_edit_text);
-					EditText productEditText = (EditText) findViewById(R.id.product_edit_text);
-					// TODO: add message2.0 event
-					// GrowthAnalytics.getInstance().purchase(Integer.valueOf(priceEditText.getText().toString()),
-					// "item",
-					// productEditText.getText().toString());
-				} catch (NumberFormatException e) {
-					Log.w("Grwothbeat Sample", "Input value error :" + e.getMessage());
-				}
-			}
-		});
-	}
-
-	@Override
-	public void onStart() {
-		super.onStart();
-	}
-
-	@Override
-	public void onStop() {
-		super.onStop();
-	}
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
 }
