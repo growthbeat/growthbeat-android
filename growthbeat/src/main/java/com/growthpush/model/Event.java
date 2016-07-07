9441b773687d909655332c2b34be611699ec0f3b
Ogawa Shigeru <ogawa@sirok.co.jp>
2016-07-07 12:17:37 +0900

    fix format

--- growthbeat/src/main/java/com/growthpush/model/Event.java
+++ growthbeat/src/main/java/com/growthpush/model/Event.java
@@ -1,130 +1,130 @@
 package com.growthpush.model;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import com.growthbeat.model.Model;
 import com.growthbeat.utils.JSONObjectUtils;
 import com.growthpush.GrowthPush;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Event extends Model {
 
-	private int goalId;
+    private int goalId;
 
-	private String clientId;
+    private String clientId;
 
-	private long timestamp;
+    private long timestamp;
 
-	private String value;
+    private String value;
 
-	public Event() {
-		super();
-	}
+    public Event() {
+        super();
+    }
 
-	public Event(JSONObject jsonObject) {
-		this();
-		setJsonObject(jsonObject);
-	}
+    public Event(JSONObject jsonObject) {
+        this();
+        setJsonObject(jsonObject);
+    }
 
-	public static Event create(String clientId, String applicationId, String credentialId, EventType type, String name, String value) {
+    public static Event create(String clientId, String applicationId, String credentialId, EventType type, String name, String value) {
 
-		Map<String, Object> params = new HashMap<String, Object>();
-		if (clientId != null)
-			params.put("clientId", clientId);
-		if (applicationId != null)
-			params.put("applicationId", applicationId);
-		if (credentialId != null)
-			params.put("credentialId", credentialId);
-		if (type != null)
-			params.put("type", type.toString());
-		if (name != null)
-			params.put("name", name);
-		if (value != null)
-			params.put("value", value);
+        Map<String, Object> params = new HashMap<String, Object>();
+        if (clientId != null)
+            params.put("clientId", clientId);
+        if (applicationId != null)
+            params.put("applicationId", applicationId);
+        if (credentialId != null)
+            params.put("credentialId", credentialId);
+        if (type != null)
+            params.put("type", type.toString());
+        if (name != null)
+            params.put("name", name);
+        if (value != null)
+            params.put("value", value);
 
-		JSONObject jsonObject = GrowthPush.getInstance().getHttpClient().post("4/events", params);
-		if (jsonObject == null)
-			return null;
+        JSONObject jsonObject = GrowthPush.getInstance().getHttpClient().post("4/events", params);
+        if (jsonObject == null)
+            return null;
 
-		return new Event(jsonObject);
+        return new Event(jsonObject);
 
-	}
+    }
 
-	public int getGoalId() {
-		return goalId;
-	}
+    public int getGoalId() {
+        return goalId;
+    }
 
-	public void setGoalId(int goalId) {
-		this.goalId = goalId;
-	}
+    public void setGoalId(int goalId) {
+        this.goalId = goalId;
+    }
 
-	public String getClientId() {
-		return clientId;
-	}
+    public String getClientId() {
+        return clientId;
+    }
 
-	public void setClientId(String clientId) {
-		this.clientId = clientId;
-	}
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
+    }
 
-	public long getTimestamp() {
-		return timestamp;
-	}
+    public long getTimestamp() {
+        return timestamp;
+    }
 
-	public void setTimestamp(long timestamp) {
-		this.timestamp = timestamp;
-	}
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
 
-	public String getValue() {
-		return value;
-	}
+    public String getValue() {
+        return value;
+    }
 
-	public void setValue(String value) {
-		this.value = value;
-	}
+    public void setValue(String value) {
+        this.value = value;
+    }
 
-	@Override
-	public JSONObject getJsonObject() {
-		JSONObject jsonObject = new JSONObject();
+    @Override
+    public JSONObject getJsonObject() {
+        JSONObject jsonObject = new JSONObject();
 
-		try {
-			jsonObject.put("goalId", getGoalId());
-			jsonObject.put("clientId", getClientId());
-			jsonObject.put("timestamp", getTimestamp());
-			if (value != null)
-				jsonObject.put("value", getValue());
-		} catch (JSONException e) {
-			return null;
-		}
+        try {
+            jsonObject.put("goalId", getGoalId());
+            jsonObject.put("clientId", getClientId());
+            jsonObject.put("timestamp", getTimestamp());
+            if (value != null)
+                jsonObject.put("value", getValue());
+        } catch (JSONException e) {
+            return null;
+        }
 
-		return jsonObject;
-	}
+        return jsonObject;
+    }
 
-	@Override
-	public void setJsonObject(JSONObject jsonObject) {
+    @Override
+    public void setJsonObject(JSONObject jsonObject) {
 
-		if (jsonObject == null)
-			return;
+        if (jsonObject == null)
+            return;
 
-		try {
-			if (JSONObjectUtils.hasAndIsNotNull(jsonObject, "goalId"))
-				setGoalId(jsonObject.getInt("goalId"));
-			if (JSONObjectUtils.hasAndIsNotNull(jsonObject, "clientId"))
-				setClientId(jsonObject.getString("clientId"));
-			if (JSONObjectUtils.hasAndIsNotNull(jsonObject, "timestamp"))
-				setTimestamp(jsonObject.getLong("timestamp"));
-			if (JSONObjectUtils.hasAndIsNotNull(jsonObject, "value"))
-				setValue(jsonObject.getString("value"));
-		} catch (JSONException e) {
-			throw new IllegalArgumentException("Failed to parse JSON.");
-		}
-	}
+        try {
+            if (JSONObjectUtils.hasAndIsNotNull(jsonObject, "goalId"))
+                setGoalId(jsonObject.getInt("goalId"));
+            if (JSONObjectUtils.hasAndIsNotNull(jsonObject, "clientId"))
+                setClientId(jsonObject.getString("clientId"));
+            if (JSONObjectUtils.hasAndIsNotNull(jsonObject, "timestamp"))
+                setTimestamp(jsonObject.getLong("timestamp"));
+            if (JSONObjectUtils.hasAndIsNotNull(jsonObject, "value"))
+                setValue(jsonObject.getString("value"));
+        } catch (JSONException e) {
+            throw new IllegalArgumentException("Failed to parse JSON.");
+        }
+    }
 
-	public enum EventType {
-		custom,
-		message
-	}
+    public enum EventType {
+        custom,
+        message
+    }
 
 }
